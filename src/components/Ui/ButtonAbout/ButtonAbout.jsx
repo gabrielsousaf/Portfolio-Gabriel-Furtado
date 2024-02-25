4a0dc6061f2903ebd06dcfc9dac2c8f0f0c56c89
GABRIEL DE SOUZA FURTADO <121953504+gabrielsousaf@users.noreply.github.com>
2024-02-25 20:16:25 -0300

    Update ButtonAbout.jsx

--- src/components/Ui/ButtonAbout/ButtonAbout.jsx
+++ src/components/Ui/ButtonAbout/ButtonAbout.jsx
@@ -17,7 +17,7 @@ const ButtonAbout = () => {
 
   return (
     <Buttons>
-      <a href="https://drive.google.com/file/d/1eUOin6BFkSClpazqolH1f74n1LlyrXgU/view?usp=sharing" target="_blank" rel="noreferrer">Curriculo</a>
+      <a href="https://drive.google.com/file/d/1vT_i2ArchXdab4uJW2R7yWunjEfaQ9yJ/view?usp=sharing" target="_blank" rel="noreferrer">Curriculo</a>
       <a onClick={handleMenuItemClick} href="#skills">Skills</a>
     </Buttons>
   )
